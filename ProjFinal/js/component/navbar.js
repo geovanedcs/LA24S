b9739485713a6b30df60cf64da086c8bdaa06bf1
Geovane Soares <57137423+geovanedcs@users.noreply.github.com>
2023-11-01 11:24:21 +0000

    navbar component start

---
+++ ProjFinal/js/component/navbar.js
@@ -0,0 +1,9 @@
+export function navbarComponent(){
+    const navbarNav = document.createElement('nav');
+    navbarNav.classList.add('navbar', 'navbar-expand-lg', 'navbar-light', 'bg-white');
+
+    const divNavbar = document.createElement('div');
+    divNavbar.classList.add('container-fluid');
+
+    
+}
